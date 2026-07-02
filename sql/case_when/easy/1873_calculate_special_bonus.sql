e90f75f6c500bf6b0caafceb99f2a913b79c0d85
Jonas Kwan <jonastheitguy@gmail.com>
2026-07-02 17:48:23 +0800

    Create 1873_calculate_special_bonus.sql

---
+++ sql/case_when/easy/1873_calculate_special_bonus.sql
@@ -0,0 +1,54 @@
+/*
+Problem Description: 
+
+https://leetcode.com/problems/calculate-special-bonus/description/
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key (column with unique values) for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+ 
+
+Write a solution to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee's name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+
+The result format is in the following example.
+
+*/
+
+
+
+
+
+
+
+
+
+
+# Write your MySQL query statement below
+SELECT 
+employee_id, 
+CASE 
+    WHEN employee_id % 2 != 0 AND name NOT LIKE 'M%' THEN salary
+    ELSE 0
+END bonus
+FROM Employees
+ORDER BY employee_id
+
+/* Can use Mod instead*/
+/*
+SELECT employee_id,
+              CASE
+                 WHEN MOD(employee_id, 2) != 0  AND name NOT LIKE 'M%' THEN salary
+                 ELSE 0
+              END as bonus  
+FROM Employees;
+
+*/

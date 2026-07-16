f84e44e1419d28222dd29be2b150e7296128bbb4
Jonas Kwan <jonastheitguy@gmail.com>
2026-07-16 10:41:36 +0800

    Create 1667_fix_names_in_a_table.sql

---
+++ sql/easy/1667_fix_names_in_a_table.sql
@@ -0,0 +1,68 @@
+/*
+
+Code
+Testcase
+Testcase
+Test Result
+1667. Fix Names in a Table
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+SQL Schema
+Pandas Schema
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+
+*/
+
+//Method 1 
+SELECT user_id,  CONCAT(UPPER(SUBSTRING(name, 1, 1)), 
+LOWER(SUBSTRING(name, 2, LEN(name)))) as name
+FROM Users
+ORDER BY user_id
+
+//Method 2
+
+SELECT user_id, CONCAT(
+    UPPER(LEFT(name,1)),
+    LOWER(RIGHT(name, LEN(name)-1))
+) as name
+FROM Users
+ORDER BY user_id
